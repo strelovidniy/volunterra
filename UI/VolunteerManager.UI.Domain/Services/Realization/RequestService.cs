83697433aa8a16a3cc439e6ef6f25ad430b50987
Roman Danylevych <r.danylevych@brainence.com>
2023-05-27 17:12:04 +0300

    Fixed NotImplemented

--- UI/VolunteerManager.UI.Domain/Services/Realization/RequestService.cs
+++ UI/VolunteerManager.UI.Domain/Services/Realization/RequestService.cs
@@ -1,33 +1,63 @@
 ﻿using VolunteerManager.Models.Create;
 using VolunteerManager.Models.Update;
 using VolunteerManager.Models.Views;
+using VolunteerManager.UI.Domain.Http.VolunteerManagerHttpClient;
 using VolunteerManager.UI.Domain.Services.Abstraction;
 
 namespace VolunteerManager.UI.Domain.Services.Realization;
 
 internal class RequestService : IRequestService
 {
+    private readonly IVolunteerManagerHttpClient _httpClient;
+
+    public RequestService(
+        IVolunteerManagerHttpClient httpClient
+    ) => _httpClient = httpClient;
+
     public Task CreateRequestAsync(
         CreateRequestModel model,
         CancellationToken cancellationToken = default
-    ) => throw new NotImplementedException();
+    ) => _httpClient
+        .PostAsync(
+            "api/v1/requests",
+            _httpClient.CreateJsonContent(model),
+            cancellationToken
+        );
 
     public Task UpdateRequestAsync(
         UpdateRequestModel model,
         CancellationToken cancellationToken = default
-    ) => throw new NotImplementedException();
+    ) => _httpClient
+        .PutAsync(
+            "api/v1/requests",
+            _httpClient.CreateJsonContent(model),
+            cancellationToken
+        );
 
     public Task DeleteRequestAsync(
         Guid requestId,
         CancellationToken cancellationToken = default
-    ) => throw new NotImplementedException();
+    ) => _httpClient
+        .DeleteAsync(
+            $"api/v1/requests/{requestId}",
+            cancellationToken
+        );
 
     public Task CreateOrganizationRequestAsync(
         CreateOrganizationRequestModel model,
         CancellationToken cancellationToken = default
-    ) => throw new NotImplementedException();
+    ) => _httpClient
+        .PostAsync(
+            "api/v1/requests/organization",
+            _httpClient.CreateJsonContent(model),
+            cancellationToken
+        );
 
     public Task<IEnumerable<RequestView>?> GetRequestsAsync(
         CancellationToken cancellationToken = default
-    ) => throw new NotImplementedException();
+    ) => _httpClient
+        .GetAsync<IEnumerable<RequestView>>(
+            "api/v1/requests",
+            cancellationToken
+        );
 }
