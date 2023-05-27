368e8e332854bac7807331f49e479d08905e8270
Roman Sadokha <roman.sadokha01@gmail.com>
2023-05-27 17:14:29 +0300

    Added initial ChatGpt API requiests

--- AI/main.py
+++ AI/main.py
@@ -1,7 +1,23 @@
+import os
+import openai
 from flask import Flask
+
+openai.api_key = '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'
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  temperature=0.7,
+  messages=[
+    {"role": "user", "content": "Generate the description to the volunteering post about opened fundraising for armor"}
+  ]
+)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hello World</h1>"
+    return completion.choices[0].message.content
+
+@app.route('/generate_description')
+def generate_description():
+    return completion.choices[0].message.content
