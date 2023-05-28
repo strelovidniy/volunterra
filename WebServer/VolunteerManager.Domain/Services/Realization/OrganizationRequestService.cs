1de5d9ff171a3e239fa8f1e7a894fa0a788c8b61
Nazariy Chtvertukha <nazariy.chetvertukha@gmail.com>
2023-05-28 10:02:01 +0300

    merge

--- WebServer/VolunteerManager.Domain/Services/Realization/OrganizationRequestService.cs
+++ WebServer/VolunteerManager.Domain/Services/Realization/OrganizationRequestService.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
 using EntityFrameworkCore.RepositoryInfrastructure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using VolunteerManager.Data.Entities;
 using VolunteerManager.Data.Enums.RichEnums;
+using VolunteerManager.Domain.Extensions;
 using VolunteerManager.Domain.Models.ViewModels;
 using VolunteerManager.Domain.Services.Abstraction;
+using VolunteerManager.Domain.Settings.Abstraction;
 using VolunteerManager.Models.Create;
 using VolunteerManager.Models.Views;
 
@@ -14,20 +17,26 @@ public class OrganizationRequestService : IOrganizationRequestService
 {
     private readonly IRepository<OrganizationRequest> _organizationRequestRepository;
     private readonly IRepository<Organization> _organizationRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly IEmailService _emailService;
+    private readonly IUrlSettings _urlSettings;
     private readonly IMapper _mapper;
 
     public OrganizationRequestService(
         IRepository<OrganizationRequest> organizationRequestRepository,
         IMapper mapper,
         IRepository<Organization> organizationRepository,
-        IEmailService emailService
+        IEmailService emailService,
+        IHttpContextAccessor httpContextAccessor,
+        IUrlSettings urlSettings
     )
     {
         _organizationRequestRepository = organizationRequestRepository;
         _mapper = mapper;
         _organizationRepository = organizationRepository;
         _emailService = emailService;
+        _httpContextAccessor = httpContextAccessor;
+        _urlSettings = urlSettings;
     }
 
     public async Task CreateOrganizationRequestAsync(
@@ -57,9 +66,38 @@ public class OrganizationRequestService : IOrganizationRequestService
         );
     }
 
+    public async Task UploadRequestImageAsync(byte[] avatar, CancellationToken cancellationToken = default)
+    {
+        var organizationRequest = _organizationRequestRepository
+            .Query()
+            .FirstOrDefault(user => user.Id == _httpContextAccessor.GetCurrentUserId());
+
+        var avatarName = organizationRequest!.Id.ToString();
+
+        var combine = Path.Combine(Directory.GetCurrentDirectory(),
+            "Files",
+            "Requests");
+
+        var filePath = Path.Combine(
+            combine,
+            avatarName
+        );
+
+        if (!Directory.Exists(combine))
+        {
+            Directory.CreateDirectory(combine);
+        }
+
+        await File.WriteAllBytesAsync(filePath, avatar, cancellationToken);
+
+        organizationRequest.ImageDataUrl = _urlSettings.WebApiUrl + "api/v1/organizationRequests/images/" + avatarName;
+
+        await _organizationRequestRepository.SaveChangesAsync(cancellationToken);
+    }
+
     public async Task<OrganizationRequestView?> GetOrganizationRequestAsync(Guid organizationRequestId, CancellationToken cancellationToken = default)
     {
-        var organizationRequest = await _organizationRepository
+        var organizationRequest = await _organizationRequestRepository
             .Query()
             .FirstOrDefaultAsync(x => x.Id == organizationRequestId, cancellationToken: cancellationToken);
 
